a76573301b5de2d14020c7ee2fe02ccc15e14ebf
Benjamin <kontakt@benjamin-halbrock.de>
2018-08-02 23:29:12 +0200

    is now working with mcs and using libsu

--- app/src/main/java/com/google/android_quick_settings/MainActivity.java
+++ app/src/main/java/com/google/android_quick_settings/MainActivity.java
@@ -16,12 +16,68 @@ package com.google.android_quick_settings;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.topjohnwu.superuser.Shell;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    private Shell.Container container;
+    private Thread thread = new Thread() {
+        @Override
+    public void run() {
+        try {
+            while (!thread.isInterrupted()) {
+                Thread.sleep(1000);
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        // update TextView here!
+                        doSomething();
+                    }
+                });
+            }
+        } catch (InterruptedException e) {
+        }
+    }
+    };
 
-    @Override
+    private View.OnClickListener awesomeOnClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            doSomething();
+        }
+    };
+
+
+        @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // Assign the container with a pre-configured Container
+        container = Shell.Config.newContainer();
         setContentView(R.layout.activity_main);
+        TextView textView = (TextView) findViewById(R.id.mcsInfo);
+        //textView.setOnClickListener(awesomeOnClickListener);
+        thread.start();
+
+    }
+
+    protected void doSomething(){
+        List<String> output = Shell.su("mcs -i").exec().getOut();
+
+        TextView textView = (TextView) findViewById(R.id.mcsInfo);
+        textView.setText("");
+        for(String tmp:output){
+            textView.append(tmp + "\n");
+        }
+
+    }
+
+    @Override
+    public void onRestart() {
+        super.onRestart();
+        doSomething();
     }
 }
--- app/src/start/java/com/google/android_quick_settings/QuickSettingsService.java
+++ app/src/start/java/com/google/android_quick_settings/QuickSettingsService.java
@@ -34,22 +34,33 @@ import android.graphics.drawable.Icon;
 import android.service.quicksettings.Tile;
 
 
+import com.topjohnwu.superuser.Shell;
+
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.util.List;
 import java.util.Locale;
 
 
 @SuppressLint("Override")
 @TargetApi(Build.VERSION_CODES.N)
 public class QuickSettingsService
-   extends android.service.quicksettings.TileService {
+        extends android.service.quicksettings.TileService {
 
     private static final String SERVICE_STATUS_FLAG = "serviceStatus";
     private static final String PREFERENCES_KEY = "com.google.android_quick_settings";
+    private Shell.Container container;
 
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        // Assign the container with a pre-configured Container
+        container = Shell.Config.newContainer();
+    }
 
     /**
      * Called when the tile is added to the Quick Settings.
+     *
      * @return TileService constant indicating tile state
      */
     @Override
@@ -94,20 +105,18 @@ public class QuickSettingsService
     @Override
     public void onTileRemoved() {
         Log.d("QS", "Tile removed, charging enabled");
-        //enable charging
-        enableCharging();
+        //enable mcs
+        enableMcs();
     }
 
 
-    private void switchState(){
-        boolean state = getServiceStatus();
-
-        if (state){
-            //disable charging
-            disableCharging();
-        } else {
-            //enable charging
+    private void switchState() {
+        //boolean state = getServiceStatus();
+        if (mcsActive()) {
             enableCharging();
+
+        } else {
+            enableMcs();
         }
 
         //https://stackoverflow.com/questions/8369718/sleep-function-in-android-program
@@ -116,7 +125,7 @@ public class QuickSettingsService
             @Override
             public void run() {
                 try {
-                    Thread.sleep(1000);
+                    Thread.sleep(500);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
@@ -136,14 +145,60 @@ public class QuickSettingsService
 
     }
 
-
-    private void enableCharging(){
-        setChargingState(true);
+    public boolean mcsActive() {
+        // List<String> output = Shell.su("find /dev/block -iname boot").exec().getOut();
+        List<String> output = Shell.su("mcs -i").exec().getOut();
+        for (String tmp : output) {
+            if (tmp.contains("pause_svc=false")) {
+                return true;
+            }
+        }
+        return false;
     }
 
-    private void disableCharging(){
-        setChargingState(false);
+    public void enableCharging() {
+        //mcs -e
+        Log.d("QS", "Enable Charging called");
+        List<String> output = Shell.su("mcs -e").exec().getOut();
     }
+
+
+    public void enableMcs() {
+        //mcs -s
+        List<String> output = Shell.su("mcs -s").exec().getOut();
+        Log.d("QS", "Enable MCS called");
+    }
+
+    /**
+     * @return -1 on error
+     */
+    public Integer getModCapacity() {
+        List<String> output = Shell.su("cat /sys/class/power_supply/gb_battery/capacity").exec().getOut();
+        for (String tmp : output) {
+            if (tmp.contains("No such file or directory")) {
+                Log.e("QS", "No battery-mod connected");
+                continue;
+            }
+
+            try {
+                int charge = Integer.parseInt(tmp);
+                Log.d("QS", "Mod Charge: " + charge + "%");
+                return charge;
+            } catch (java.lang.NumberFormatException e) {
+                Log.e("QS", "getModCapacity failed", e);
+                //nothing
+            }
+        }
+        return null;
+    }
+
+//    private void enableCharging(){
+//        setChargingState(true);
+//    }
+
+//    private void disableCharging(){
+//        setChargingState(false);
+//    }
 
     /*private void setChargingState2(boolean enableCharging){
         //doesn't work
@@ -185,13 +240,13 @@ public class QuickSettingsService
     }
 */
 
-    private void setChargingState(boolean enableCharging){
+    private void setChargingState(boolean enableCharging) {
         //https://gist.github.com/rosterloh/c4bd02bed8c5e7bd47c5
 
         int value = 1;
 
 
-        if (enableCharging){
+        if (enableCharging) {
             Log.d("QS", "Setting charging status: enabled");
             value = 1;
 
@@ -203,7 +258,7 @@ public class QuickSettingsService
 
 
         try {
-            String[] test = new String[] {"su", "-c", command};
+            String[] test = new String[]{"su", "-c", command};
             Runtime.getRuntime().exec(test);
 
 
@@ -219,38 +274,56 @@ public class QuickSettingsService
     private void updateTile() {
 
         Tile tile = this.getQsTile();
-        boolean isActive = getServiceStatus();
+//        boolean isActive = getServiceStatus();
+        boolean isActive = mcsActive();
 
         Icon newIcon;
         String newLabel;
         int newState;
-
+        Integer capacity = getModCapacity();
         // Change the tile to match the service status.
         if (isActive) {
+            if (capacity != null) {
+                newLabel = String.format(Locale.US,
+                        "%s - %d%%",
+                        getString(R.string.mcs_active),
+                        capacity);
+            } else {
+                newLabel = String.format(Locale.US,
+                        "%s",
+                        getString(R.string.mcs_active));
+            }
 
-            newLabel = String.format(Locale.US,
-                    "%s %s",
-                    getString(R.string.tile_label),
-                    getString(R.string.service_active));
 
             newIcon = Icon.createWithResource(getApplicationContext(),
-                    R.drawable.battery_charging_60);
+                    R.drawable.battery_60);
 
             newState = Tile.STATE_ACTIVE;
 
         } else {
-            newLabel = String.format(Locale.US,
-                    "%s %s",
-                    getString(R.string.tile_label),
-                    getString(R.string.service_inactive));
+
+            if (capacity != null) {
+                newLabel = String.format(Locale.US,
+                        "%s - %d%%",
+                        getString(R.string.mcs_inactive),
+                        capacity
+                );
+            } else {
+                newLabel = String.format(Locale.US,
+                        "%s",
+                        getString(R.string.mcs_inactive)
+                );
+            }
+
             newIcon = Icon.createWithResource(getApplicationContext(),
-                    R.drawable.battery_60);
+                    R.drawable.battery_charging_60);
 
            /* newIcon =
                     Icon.createWithResource(getApplicationContext(),
                            android.R.drawable.ic_lock_idle_low_battery);*/
 
             newState = Tile.STATE_INACTIVE;
+            //   newState = Tile.STATE_ACTIVE;
         }
 
         // Change the UI of the tile.
@@ -263,54 +336,54 @@ public class QuickSettingsService
     }
 
 
-    private boolean getServiceStatus() {
-
-        SharedPreferences prefs =
-                getApplicationContext()
-                        .getSharedPreferences(PREFERENCES_KEY, MODE_PRIVATE);
-
-        //boolean isActive = prefs.getBoolean(SERVICE_STATUS_FLAG, false);
-
-        Log.d("QS", "requesting root");
-        boolean isActive = false;
-        try {
-            getRuntime().exec("su");
-
-            File infile;
-            FileInputStream inputStream;
-            byte[] buffer = null;
-            try {
-                infile=new File("/sys/class/power_supply/battery/charging_enabled");
-
-                if(infile.exists()){
-                    InputStream is = new FileInputStream(infile);
-                    byte[] b = new byte[is.available()];
-                    is.read(b);
-                    String fileContent = new String(b);
-                    is.close();
-
-                    //Log.d("QS", fileContent);
-                    if (fileContent.startsWith("1")){
-                        Log.d("QS", "charging is activated");
-                        isActive = true;
-                    } else {
-                        Log.d("QS", "charging is not activated");
-                        isActive = false;
-                    }
-                }
-            } catch (Exception e) {
-                Log.e("QS", "Could't read charging status", e);
-            }
-
-        } catch (IOException e) {
-            Log.e("QS", "Requesting root didn't work", e);
-        }
-
-        //not needed, yet
-        prefs.edit().putBoolean(SERVICE_STATUS_FLAG, isActive).apply();
-
-        return isActive;
-    }
+//    private boolean getServiceStatus() {
+//
+//        SharedPreferences prefs =
+//                getApplicationContext()
+//                        .getSharedPreferences(PREFERENCES_KEY, MODE_PRIVATE);
+//
+//        //boolean isActive = prefs.getBoolean(SERVICE_STATUS_FLAG, false);
+//
+//        Log.d("QS", "requesting root");
+//        boolean isActive = false;
+//        try {
+//            getRuntime().exec("su");
+//
+//            File infile;
+//            FileInputStream inputStream;
+//            byte[] buffer = null;
+//            try {
+//                infile=new File("/sys/class/power_supply/battery/charging_enabled");
+//
+//                if(infile.exists()){
+//                    InputStream is = new FileInputStream(infile);
+//                    byte[] b = new byte[is.available()];
+//                    is.read(b);
+//                    String fileContent = new String(b);
+//                    is.close();
+//
+//                    //Log.d("QS", fileContent);
+//                    if (fileContent.startsWith("1")){
+//                        Log.d("QS", "charging is activated");
+//                        isActive = true;
+//                    } else {
+//                        Log.d("QS", "charging is not activated");
+//                        isActive = false;
+//                    }
+//                }
+//            } catch (Exception e) {
+//                Log.e("QS", "Could't read charging status", e);
+//            }
+//
+//        } catch (IOException e) {
+//            Log.e("QS", "Requesting root didn't work", e);
+//        }
+//
+//        //not needed, yet
+//        prefs.edit().putBoolean(SERVICE_STATUS_FLAG, isActive).apply();
+//
+//        return isActive;
+//    }
 
 
 }
